1641af8de97485fc780031ec95cd73d278262fc5
Oluwatobi Babatunde JAmes <oluwatobi.james@yahoo.com>
2021-05-16 20:47:19 +0100

    Third commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import datetime
 import local_constants
 from flask import Flask, render_template, request, redirect, Response, session, flash
@@ -58,10 +59,15 @@ def retrieveDirectory(claims):
     return directory_names
 
 
-def blobList(prefix):
+def blobList(prefix, boolean):
     storage_client = storage.Client(project=local_constants.PROJECT_NAME)
+    delimiter=None
 
-    return storage_client.list_blobs(local_constants.PROJECT_STORAGE_BUCKET, prefix=prefix)
+    if boolean==True:
+        delimiter='/'
+        
+
+    return storage_client.list_blobs(local_constants.PROJECT_STORAGE_BUCKET, prefix=prefix, delimiter=delimiter)
 
 def delete_blob(blob_name):
    
@@ -94,10 +100,10 @@ def downloadBlob(filename):
     blob = storage.Blob(filename, bucket)
     return blob.download_as_bytes() 
 
-@app.route('/addDirectory', methods=['POST', 'GET'])
-def addDirectoryPageHandler():
+@app.route('/addDirectory/<path:name>', methods=['POST', 'GET'])
+def addDirectoryPageHandler(name):
     
-    return render_template('addDirectory.html')
+    return render_template('addDirectory.html', path=name)
 
 @app.route('/booking/<int:id>', methods=['POST', 'GET'])
 def bookingPageHandler(id):
@@ -127,7 +133,7 @@ def root():
             session['location']=session['email']+'/'
             mylist=user_info['directory_list_keys']
 
-            blob_list = blobList(None) 
+            blob_list = blobList(None, False) 
             for i in blob_list:
                 if i.name == session['email']+'/':
                     directory_list.append(i)
@@ -136,14 +142,14 @@ def root():
 
             if len(directory_list)==0:
                 addDirectory(session['email']+'/')
-                blob_list = blobList(None) 
+                blob_list = blobList(None, False) 
                 for i in blob_list:
                     if i.name == session['email']+'/':
                         directory_list.append(i)
                         session['location']=i
                         
 
-            print(directory_list)
+            
             
         except ValueError as exc: 
             error_message = str(exc)
@@ -152,14 +158,15 @@ def root():
 
     return render_template('indexPage.html', user_data=claims, error_message=error_message, user_info=user_info, root=session['email']+'/', directory_list=directory_list)
 
-@app.route('/add_directory', methods=['POST']) 
-def addDirectoryHandler():
+@app.route('/add_directory/<path:name>/', methods=['POST']) 
+def addDirectoryHandler(name):
     id_token = request.cookies.get("token") 
     error_message = None
     claims = None
     times = None
     user_info = None
     directory=[]
+    count=0
     if id_token:
         try:
             claims = google.oauth2.id_token.verify_firebase_token(id_token, firebase_request_adapter)
@@ -168,8 +175,10 @@ def addDirectoryHandler():
             if directory_name == '' or directory_name[len(directory_name) - 1] != '/':
                 flash('A directory should have a directory name followed by a /')
                 return render_template('addDirectory.html')
-            
-            directory_name = session['location']+directory_name
+            name=name+'/'
+
+            directory_name = name+directory_name
+            print(directory_name)
             user_info = retrieveUserInfo(claims)
             directory=user_info['directory_list_keys']
 
@@ -180,22 +189,24 @@ def addDirectoryHandler():
 
             addDirectory(directory_name)
             directory.append(directory_name)
-            
+            print(directory)
             user_info.update({
                 'directory_list_keys': directory
             })
             datastore_client.put(user_info)
-
+            count = directory_name.count('/')
             createDirectory(claims, directory_name)
             flash('You have successfully created a new directory')
             session['location']=directory_name
-
+            directory.remove(directory_name)
+            print('this is addDi',session['location'])
+       
         except ValueError as exc: 
             error_message = str(exc)
 
-    return render_template('directoryPage.html', directory_list=directory, user_info=user_info)
+    return render_template('directoryPage.html', directory_list=directory, user_info=user_info, count=count)
 
-@app.route('/show/<string:name>/', methods=['POST', 'GET'])
+@app.route('/show/<path:name>/', methods=['POST', 'GET'])
 def showDirectory(name):
     id_token = request.cookies.get("token") 
     error_message = None
@@ -203,24 +214,62 @@ def showDirectory(name):
     name = name+'/'
     user_info = None
     directory_list=[]
+    count=0
+    prefix=None
 
     if id_token:
         try:
             claims = google.oauth2.id_token.verify_firebase_token(id_token, firebase_request_adapter)
             user_info = retrieveUserInfo(claims)
            
-            blob_list = blobList(name)
+            blob_list = blobList(name, True)
             for i in blob_list:
-                if i.name[len(i.name)-1]=='/' and i.name != name:
-                    print(i.name)
-                    directory_list.append(i)
+                if i.name[len(i.name)-1]=='/':
                     session['location']=name
-                
+            for prefix in blob_list.prefixes:
+                directory_list.append(prefix)
+                count = prefix.count('/')
+                print(session['location'])     
 
         except ValueError as exc: 
             error_message = str(exc)
     
-    return render_template('directoryPage.html', directory_list=directory_list, user_info=user_info)
+    return render_template('directoryPage.html', directory_list=directory_list, user_info=user_info, count=count, path=session['location'])
+
+@app.route('/way/<path:vn>', methods=['POST', 'GET'])
+def changeDirectory(vn):
+    id_token = request.cookies.get("token") 
+    error_message = None
+    claims = None
+    name = session['location']
+    user_info = None
+    directory_list=[]
+    count=0
+    prefix=None
+
+    if id_token:
+        try:
+            claims = google.oauth2.id_token.verify_firebase_token(id_token, firebase_request_adapter)
+            user_info = retrieveUserInfo(claims)
+            myList=''
+            count = vn.count('/')
+            temp=vn.split('/')
+            
+            for i in range(count-1):
+                myList=myList+temp[i]+"/"
+
+            blob_list = blobList(myList, True)
+            for i in blob_list:
+                if i.name[len(i.name)-1]=='/':
+                    sc=0
+            for prefix in blob_list.prefixes:
+                directory_list.append(prefix) 
+            count=count-1
+        except ValueError as exc: 
+            error_message = str(exc)
+    
+    return render_template('directoryPage.html', directory_list=directory_list, user_info=user_info, count=count, path=myList)
+
 
 @app.route('/delete/<path:name>', methods=['POST', 'GET'])
 def deleteDirectory(name):
@@ -231,26 +280,51 @@ def deleteDirectory(name):
     name = name
     user_info = None
     directory_list=[]
+    temp=[]
 
     if id_token:
         try:
             claims = google.oauth2.id_token.verify_firebase_token(id_token, firebase_request_adapter)
             user_info = retrieveUserInfo(claims)
             print('this is'+name)
-            blob_list = blobList(name)
+            blob_list = blobList(name, None)
             for i in blob_list:
                 if i.name[len(i.name)-1]=='/' and i.name != name:
                         print(i.name)
                         directory_list.append(i)
                         #session['location']=name
             
-            print(directory_list)
-            print(session['location'])
+            if len(directory_list)!=0:
+                flash('This directory has contain so cannot delete it ')
+                return render_template('directoryPage.html', directory_list=directory_list)
 
+            mylist = user_info['directory_list_keys']
+            
+            for n in range(len(mylist)):
+                if not name.find(mylist[n])>=0:
+                    temp.append(mylist[n])
+            
+            print(temp)
+            delete_blob(name)
+
+            user_info.update({
+                'directory_list_keys':temp
+            })
+
+            datastore_client.put(user_info) 
+            
+            directory_list=[]
+            blob_list = blobList(session['location'], None)
+            for i in blob_list:
+                if i.name[len(i.name)-1]=='/' and i.name != name:
+                    print(i.name)
+                    directory_list.append(i)
+                    session['location']=name
+        
         except ValueError as exc: 
             error_message = str(exc)
     
-    return render_template('directoryPage.html')
+    return redirect('/')
 
 
 @app.route('/upload_file', methods=['post']) 
